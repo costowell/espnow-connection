024c36d977e1224275ce757fe042005dc97e4619
Cole Stowell <cole@stowell.pro>
2023-11-29 13:12:00 -0500

    Yay it works

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_println::println;
+use esp_wifi::{
+    current_millis,
+    esp_now::{PeerInfo, BROADCAST_ADDRESS},
+    EspWifiInitFor,
+};
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay, Rng};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
+
+    let timer = hal::timer::TimerGroup::new(peripherals.TIMG1, &clocks).timer0;
+    let init = esp_wifi::initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+
+    let wifi = peripherals.WIFI;
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
+
+    println!("esp-now version {}", esp_now.get_version().unwrap());
+
+    let mut next_send_time = current_millis() + 5 * 1000;
+    loop {
+        let r = esp_now.receive();
+        if let Some(r) = r {
+            println!("Received {:?}", r);
+
+            if r.info.dst_address == BROADCAST_ADDRESS {
+                if !esp_now.peer_exists(&r.info.src_address) {
+                    esp_now
+                        .add_peer(PeerInfo {
+                            peer_address: r.info.src_address,
+                            lmk: None,
+                            channel: None,
+                            encrypt: false,
+                        })
+                        .unwrap();
+                }
+                let status = esp_now
+                    .send(&r.info.src_address, b"Hello Peer")
+                    .unwrap()
+                    .wait();
+                println!("Send hello to peer status: {:?}", status);
+            }
+        }
+
+        if current_millis() >= next_send_time {
+            next_send_time = current_millis() + 5 * 1000;
+            println!("Send");
+            let status = esp_now
+                .send(&BROADCAST_ADDRESS, b"0123456789")
+                .unwrap()
+                .wait();
+            println!("Send broadcast status: {:?}", status)
+        }
+    }
+}
